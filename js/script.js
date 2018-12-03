07f8fc894a8caf19c5e3722dd8b9293f75504584
Andestrial <45152356+Andestrial@users.noreply.github.com>
2018-12-03 03:07:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,3 +18,79 @@ menu.onclick = 	function myFanction(){
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
+
+
+
+
+$(function(){
+    $('#exampleModal').arcticmodal();
+});
+
+
+
+
+$(function() { 
+$(window).scroll(function() { 
+if($(this).scrollTop() != 0) { 
+$('#totop').fadeIn(); 
+} else { 
+$('#totop').fadeOut(); 
+} 
+});
+$('#totop').click(function() { 
+$('body,html').animate({scrollTop:0},700); 
+}); 
+});
+
+(function($){
+
+var files;
+
+$('input[type=file]').on('change', function(){
+  files = this.files;
+});
+
+$('.upload_files').on( 'click', function( event ){
+
+  event.stopPropagation();
+  event.preventDefault(); 
+
+  if( typeof files == 'undefined' ) return;
+
+  var data = new FormData();
+  $.each( files, function( key, value ){
+    data.append( key, value );
+  });
+
+  data.append( 'my_file_upload', 1 );
+
+  $.ajax({
+    url         : 'pages/submit.php',
+    type        : 'POST',
+    data        : data,
+    cache       : false,
+    dataType    : 'json',
+    processData : false,
+    contentType : false,
+    success     : function( respond, status, jqXHR ){
+
+      if( typeof respond.error === 'undefined' ){
+        var files_path = respond.files;
+        var html = '';
+        $.each( files_path, function( key, val ){
+           html += val +'<br>';
+        } )
+        $('.ajax-reply').html( html );
+      }
+      else {
+        console.log('ОШИБКА: ' + respond.error );
+      }
+    },
+    error: function( jqXHR, status, errorThrown ){
+      console.log( 'ОШИБКА AJAX запроса: ' + status, jqXHR );
+    }
+  });
+});
+})(jQuery)
+
+
